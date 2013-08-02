e284405ec628dd97fa2afaa49310407a87140eec
hyqhyq_3 <i@hyq.me>
2013-08-02 13:28:05 +0800

    add wtl not found error

--- cmake/FindWTL.cmake
+++ cmake/FindWTL.cmake
@@ -6,9 +6,11 @@ find_path(WTL_ROOT_DIR
 	
 
 if(WTL_ROOT_DIR)
-	set(WTL_INCLUDE_DIRS "${WTL_ROOT_DIR}/Include")
+	set(WTL_INCLUDE_DIRS "${WTL_ROOT_DIR}")
 	message("found wtl at ${WTL_ROOT_DIR}")
 else()
 	set(WTL-NOTFOUND)
-	message("cannot found WTL")
+	if(WTL_FIND_REQUIRED)
+		message(SEND_ERROR "cannot find WTL, Please install WTL 8.1 and set WTL_ROOT to the WTL include path")
+	endif()
 endif()
